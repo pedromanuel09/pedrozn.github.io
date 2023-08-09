505ee2cb0416ae429c91270c9b98321e1f7f0cba
pedromanuel09 <trujillotasaicopedro@gmail.com>
2023-08-08 19:50:21 -0500

    initial commit

---
+++ script..js
@@ -0,0 +1,8 @@
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach((box) => {
+  box.addEventListener('click', () => {
+    boxes.forEach((b) => b.classList.remove('active'));
+    box.classList.add('active');
+  });
+});
